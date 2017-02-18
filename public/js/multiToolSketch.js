60b9e2d7175cd4b6e94e80deabfc213b36608939
Conor Kelly <ckelly@ckellydesign.net>
2017-02-18 10:16:27 -0800

    Handshake between devices

--- public/js/multiToolSketch.js
+++ public/js/multiToolSketch.js
@@ -17,16 +17,43 @@
 // Main JS for detecting and sendng events, and telling p5 to do things
 function MultiTool () {
 	var self = this;
-	
+
+
+	this.preInit = function () {
+		$('#checkPin').on('click', function (e) {
+			e.preventDefault();
+			var pin = $('#prinfield').val();
+			self.socket.emit('check-pin', { pin : Number(pin) });
+		});
+
+	};
+
+	this.init = function () {
+		self.reconnectMarkup = $('#container').html();
+		$('#container').html('<h1>Connected!!</h1>');
+
+	}
+
+	this.subscribe = function () {
+
+	}
 
 	// initialize IO
 	this.socket = io();
 
 	// When connected, send content to server for Dorthy
-	this.socket.on('connect', function(){
-		self.socket.emit('multi-tool-init');
+	this.socket.on('connect', this.preInit);
+	this.socket.once('pin-checked', function (payload) {
+		
+		if (!payload.bool) {
+			alert("No Matching Pin: ", $('#prinfield').val());
+			self.socket.on('connect', self.preInit);
+		} else {
+			console.log('Matching pin - ', payload.pin);
+			self.socket.emit('multi-tool-init', { pin : payload.pin });
+			self.init();
+		}
 	});
-
 
 }
 
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var path = require('path');
 var _ = require('underscore');
 var app = express();
 
-
 // Static Express Server Settings
 app.use(express.static('./public'));
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+app.get('/mulitool', function (req, res) {
+	res.sendFile(path.resolve(__dirname, 'public', 'multiTool.html'));
 });
 
 // start up the server on port 3000
@@ -16,6 +18,8 @@ var server = app.listen(process.env.PORT || 8000, function() {
 });
 
 
+var counterTops = [];
+var multiTools = [];
 
 
 
@@ -27,18 +31,52 @@ io.sockets.on('connection', function (socket) {
 
 	// Listen for disconnection from either Oz or Dorthy
 	socket.once('disconnect', function() {
-		console.log('connection disconnected');
+
+
+		// var oldChef = _.findWhere(counterTops, { id : this.id });
+		// if (oldChef) {
+
+		// }
+
+		// var oldTool 
+
+
 		socket.disconnect();
 	})
 
 	/// Listen for new CounterTop
 	socket.on('counter-top-init', function () {
-		console.log('new CounterTop');
+		var newChef = {
+			id: this.id,
+			pin: (Math.floor(Math.random()*90000) + 10000),
+			multiToolSocket: null,
+			counterTopSocket: this
+		}
+
+		counterTops.push(newChef);
+		this.emit('session-started', { pin : newChef.pin });
 	});
 
 	// Listen for new MultiTool
-	socket.on('multi-tool-init', function () {
-		console.log('new MultiTool!');
+	socket.on('check-pin', function (payload) {
+		var isMatch = _.findWhere(counterTops, { pin : payload.pin });
+		this.emit('pin-checked', { bool : !!isMatch, pin : payload.pin });
+	});
+
+	socket.on('multi-tool-init', function (payload) {
+		 console.log('new MultiTool -', payload.pin);
+			var chef = _.findWhere(counterTops, { pin : payload.pin });
+
+			var newTool = {
+				id: this.id,
+				pin: payload.pin,
+				multiToolSocket: this,
+				counterTopSocket: chef.counterTopSocket
+			};
+
+			multiTools.push(newTool);
+			this.emit('session-started');
+			newTool.counterTopSocket.emit('tool-connected');
 	});
 
 });
