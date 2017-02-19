4e3d550aaec31638248167ae6353dac6f31ce5ce
ckellydesign <ckellydesign.net@gmail.com>
2017-02-18 19:13:55 -0800

    Detecting Device Acceleration

--- public/js/multiToolSketch.js
+++ public/js/multiToolSketch.js
@@ -1,20 +1,5 @@
-
-
-function setup () {
-	var cc = createCanvas(windowWidth, windowHeight);
-	cc.parent('p5Container');
-	console.log('setup!');
-	frameRate(2);
-};
-
-function draw () {
-	// console.log('X Rotation', )
-};
-
-
-// p5 Code Ends Here
-
-
+var x, y, z;
+var accThreshold = 1.25;
 
 
 // Main JS for detecting and sendng events, and telling p5 to do things
@@ -28,17 +13,57 @@ function MultiTool () {
 			var pin = $('#prinfield').val();
 			self.socket.emit('check-pin', { pin : Number(pin) });
 		});
-
+		self.subscribe();
 	};
 
 	this.init = function () {
 		self.reconnectMarkup = $('#container').html();
 		// $('#container').html('<h1>Connected!!</h1>');
-		// window.canvas = new p5(Canvas);
 	}
 
 	this.subscribe = function () {
+		$(window).on('devicemotion', self.onDeviceMotion);
+	}
 
+	this.onDeviceMotion = function (e) {
+		var acc = e.originalEvent.accelerationIncludingGravity;
+
+		self.handleAcceleration(acc);
+
+		x = acc.x;
+		y = acc.y;
+		z = acc.z;
+	}
+
+	this.handleAcceleration = function (acc) {
+		// Handle Left / Right Movement
+		if (acc.x < (x - accThreshold) || acc.x > (x + accThreshold) ) {
+			if (acc.x > 0) {
+				// console.log('Right Acceleration');
+			} else {
+				// console.log('Left Acceleration');
+			}
+		}
+
+
+		// Handle Forward / Backward Movement
+		if (acc.y < (y - accThreshold) || acc.y > (y + accThreshold) ) {
+			if (acc.y > 0) {
+				// console.log('Forward Acceleration');
+			} else {
+				// console.log('Backward Acceleration');
+			}
+		}
+
+
+		// Handle Up / Down Movement
+		if (acc.z < (z - accThreshold) || acc.z > (z + accThreshold) ) {
+			if (acc.z > z) {
+				// console.log('Up Acceleration');
+			} else {
+				// console.log('Down Acceleration');
+			}
+		}
 	}
 
 	// initialize IO
